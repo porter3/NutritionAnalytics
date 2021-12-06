151e859d73c0d50d70d17caa68b8132fdc32bc44
Jake Porter <jakeporter310@gmail.com>
2021-12-05 21:18:20 -0500

    add app shell, Interceptor and Config classes

---
+++ nutritionanalytics/src/main/java/com/jakeporter/nutritionanalytics/NutritionanalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.jakeporter.nutritionanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionanalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionanalyticsApplication.class, args);
+	}
+
+}
---
+++ nutritionanalytics/src/test/java/com/jakeporter/nutritionanalytics/NutritionanalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakeporter.nutritionanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionanalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
